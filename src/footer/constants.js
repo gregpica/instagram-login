8ce91956b294791b959fcd211cf74f488469c35a
gregpica <gpica@bu.edu>
2022-02-07 12:37:07 -0800

    Create constant for footer links data

---
+++ src/footer/constants.js
@@ -0,0 +1,85 @@
+/**
+ * This data would typically exist elsewhere - perhaps fetched 
+ * from a CMS - but for this exercise I just have it hardcoded 
+ * here in a format that makes sense. The urls would also be 
+ * unique.
+ */
+
+export const FOOTER_LINKS = [
+  {
+    copy: 'Meta',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'About',
+    url: 'https://about.instagram.com/',
+  },
+  {
+    copy: 'Blog',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Jobs',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Help',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'API',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Privacy',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Terms',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Top Accounts',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Hashtags',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Locations',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Instagram Lite',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Beauty',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Dance',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Fitness',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Food & Drink',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Home & Garden',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Music',
+    url: 'https://about.facebook.com/meta'
+  },
+  {
+    copy: 'Visual Arts',
+    url: 'https://about.facebook.com/meta'
+  },
+];
