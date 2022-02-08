cedbfe3eff61647904588b8035ba3a557c26f01f
gregpica <gpica@bu.edu>
2022-02-07 17:05:39 -0800

    Add show/hide functionality to password input

--- src/form/PasswordInput.js
+++ src/form/PasswordInput.js
@@ -1,38 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+
+
+const PasswordInput = ({
+  id,
+  placeholder,
+  onChange,
+  value,
+  className,
+}) => {
+  const [isHidingPassword, setIsHidingPassword] = useState(true);
+
+  const togglePasswordHiding = () => setIsHidingPassword(!isHidingPassword);
+
+  const inputType = isHidingPassword ? 'password' : 'text';
+  const buttonCopy = isHidingPassword ? 'Show' : 'Hide';
+
+  return (
+    <div className={className}>
+      <input
+        id={id}
+        type={inputType}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+      <div onClick={togglePasswordHiding}>{buttonCopy}</div>
+    </div>
+  );
+}
 
 PasswordInput.propTypes = {
   id: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   placeholder: PropTypes.string.isRequired,
-  label: PropTypes.string,
   value: PropTypes.string.isRequired,
 };
-
-function PasswordInput({
-  id,
-  placeholder,
-  onChange,
-  value,
-  label,
-  className,
-}) {
-  return (
-    <div className={className}>
-      {label && (
-        <label htmlFor={id}>
-          <p>{label}</p>
-        </label>
-      )}
-      <input
-        id={id}
-        type="password"
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
-    </div>
-  );
-}
 
 export default PasswordInput;
--- src/log-in/LogIn.style.js
+++ src/log-in/LogIn.style.js
@@ -6,7 +6,18 @@ export const container = css`
   border: 1px solid #dbdbdb;
 `;
 
-export const formInput = css`
+export const form = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  textarea:focus, input:focus{
+    outline: none;
+  }
+`;
+
+export const formTextInput = css`
   margin-bottom: 7px;
 
   input {
@@ -17,6 +28,36 @@ export const formInput = css`
     padding: 9px 7px 8px;
     text-overflow: ellipses;
     border-radius: 3px;
+  }
+`;
+
+export const formPasswordInput = css`
+  margin-bottom: 7px;
+  display: flex;
+  align-items: center;
+
+  input {
+    width: 200px;
+    height: 18px;
+    background: #fafafa;
+    border: 1px solid #aaa4a3;
+    border-right: 0;
+    padding: 9px 7px 8px;
+    text-overflow: ellipses;
+    border-radius: 3px 0 0 3px;
+  }
+
+  div {
+    width: 52px;
+    height: 18px;
+    font-size: 14px;
+    font-weight: bold;
+    border-radius: 0 3px 3px 0;
+    border: 1px solid #aaa4a3;
+    border-left: 0;
+    padding-top: 9px;
+    padding-bottom: 8px;
+    background: #fafafa;
   }
 `;
 
