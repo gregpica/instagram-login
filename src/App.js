2335f1d77310791a08d94cd56f479690c6b48c5a
gregpica <gpica@bu.edu>
2022-02-08 21:26:48 -0800

    Create success page and set up routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import LogIn from './log-in/LogIn';
+import Success from './success/Success';
 import Footer from './footer/Footer';
+
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <LogIn />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<LogIn />} />
+          <Route exact path="/success" element={<Success />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/success/Success.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Success() {
+  return (
+    <div>
+      <h1>Success! You logged in and were redirected to a new page!</h1>
+    </div>
+  );
+}
+
+export default Success;
